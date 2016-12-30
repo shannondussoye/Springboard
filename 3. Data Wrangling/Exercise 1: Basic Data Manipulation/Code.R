0f6da2fcb65d66ccf2f1a2c7dc39eeb79d1dffae
shannondussoye <shannonduss@live.com>
2016-12-30 19:45:11 +1100

    Codes and Dir structure mod

---
+++ 3. Data Wrangling/Exercise 1: Basic Data Manipulation/Code.R
@@ -0,0 +1,5 @@
+suppressWarnings(library(dplyr))
+setwd("~/R/Projects/Springboard/3. Data Wrangling/Exercise 1: Basic Data Manipulation")
+data <- read.csv("refine.csv", sep = ",",stringsAsFactors = F)
+  
+
