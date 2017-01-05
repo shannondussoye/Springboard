ebf0928f04867d3bb2d39a35d1b296152870bc39
shannondussoye <shannonduss@live.com>
2017-01-06 01:10:10 +1100

    Commit

--- 4. Exploratory Data Analysis in R/CHIS.R
+++ 4. Exploratory Data Analysis in R/CHIS.R
@@ -45,3 +45,69 @@ ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) +
   BMI_fill+
   facet_grid(RBMI~.)+
   theme_classic()
+
+
+#---------
+#4. Alternatives
+# Plot 1 - Count histogram
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
+  geom_histogram(binwidth = 1) +
+  BMI_fill
+
+# Plot 2 - Density histogram
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
+  geom_histogram(binwidth = 1,aes(y=..density..)) +
+  BMI_fill
+
+# Plot 3 - Faceted count histogram
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
+  geom_histogram(binwidth = 1) +
+  BMI_fill+
+  facet_grid(RBMI ~ .)
+
+# Plot 4 - Faceted density histogram
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
+  geom_histogram(binwidth = 1,aes(y=..density..)) +
+  BMI_fill+
+  facet_grid(RBMI ~ .)
+
+# Plot 5 - Density histogram with position = "fill"
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
+  geom_histogram(binwidth = 1,aes(y=..density..),position="fill") +
+  BMI_fill
+
+# Plot 6 - The accurate histogram
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
+  geom_histogram(binwidth = 1,aes(y=..count../sum(..count..)),position="fill") +
+  BMI_fill
+
+
+#-------
+#5.Do Things Manually
+# An attempt to facet the accurate frequency histogram from before (failed)
+ggplot(adult, aes (x = SRAGE_P, fill= factor(RBMI))) + 
+  geom_histogram(aes(y = ..count../sum(..count..)), binwidth = 1, position = "fill") +
+  BMI_fill +
+  facet_grid(RBMI ~ .)
+
+# Create DF with table()
+DF<-table(adult$RBMI,adult$SRAGE_P)
+
+# Use apply on DF to get frequency of each group
+DF_freq<-apply(DF,2,function(x) x/sum(x))
+
+# Load reshape2 and use melt on DF to create DF_melted
+library(reshape2)
+DF_melted <- melt(DF_freq)
+DF_melted
+# Change names of DF_melted
+names(DF_melted)<-c("FILL", "X", "value")
+
+# Add code to make this a faceted plot
+ggplot(DF_melted, aes(x = X, y = value, fill = FILL)) +
+  geom_bar(stat = "identity", position = "stack") +
+  BMI_fill + 
+  facet_grid(FILL ~ .)
+
+#---------
+#6.
